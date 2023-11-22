ada85663abeda5f198a88ed22d7175fd16b8715e
Irfan Yilmaz <iy.usa@outlook.com>
2023-11-21 22:35:31 -0500

    create simple Book constructor and test it out

--- javascript.js
+++ javascript.js
@@ -2,6 +2,22 @@ addEventListener("DOMContentLoaded", () =>
 {
 
     tempDiv = document.getElementById("temp-result");
-    tempDiv.textContent = "DOM Content Loaded";
+
+
+    function Book(title, author, pages, status)
+    {
+        this.title = title;
+        this.author = author;
+        this.pages = pages + " pages";
+        this.status = (status) ? "finished/reading book" : "book unread";
+        
+        this.info = () => {
+            return this.title + " by " + this.author + ", " + this.pages + ", " + this.status;
+        }
+    }
+
+    sampleBook  = new Book("Harry Potter", "Rowling", "500", false);
+
+    tempDiv.textContent = sampleBook.info();
 
 });
