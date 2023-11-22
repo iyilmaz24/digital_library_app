4cc8af086f665d744c8f6bef744b0ef5e244d606
Irfan Yilmaz <iy.usa@outlook.com>
2023-11-22 03:59:45 -0500

    create array to store Book objects, add method to display array contents

--- javascript.js
+++ javascript.js
@@ -2,7 +2,9 @@ addEventListener("DOMContentLoaded", () =>
 {
 
     tempDiv = document.getElementById("temp-result");
+    tempLibrary = document.getElementById("temp-library");
 
+    const myLibrary = [];
 
     function Book(title, author, pages, status)
     {
@@ -10,14 +12,33 @@ addEventListener("DOMContentLoaded", () =>
         this.author = author;
         this.pages = pages + " pages";
         this.status = (status) ? "finished/reading book" : "book unread";
-        
+
         this.info = () => {
             return this.title + " by " + this.author + ", " + this.pages + ", " + this.status;
+        }
+    };
+
+    function addBook(Book)
+    {
+        myLibrary.push(Book);
+    };
+
+    function displayLibrary()
+    {
+        tempLibrary.innerHTML = "";
+        for(let i = 0; i < myLibrary.length; i++)
+        {
+            tempLibrary.innerHTML += (myLibrary[i].info()) + "<br>";
         }
     }
 
     sampleBook  = new Book("Harry Potter", "Rowling", "500", false);
+    sampleBook2 = new Book("Odin Project", "Open-Source", "3", true);
+    addBook(sampleBook);
+    addBook(sampleBook2)
+
 
     tempDiv.textContent = sampleBook.info();
+    displayLibrary()
 
 });
